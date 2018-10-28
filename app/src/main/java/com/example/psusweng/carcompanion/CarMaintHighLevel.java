33c1d70c857cb360af8de1961ea80f84afcf1659
tud12552 <jacob.s.esworthy@gmail.com>
2018-10-28 13:52:44 -0400

    Initial Commit for High Level Car Maintenance screen. Added all of the interfaces to the High Level Car Maintenance screen. Added new strings to the strings.xml Added new color "black" to colors.xml

--- app/src/main/java/com/example/psusweng/carcompanion/CarMaintHighLevel.java
+++ app/src/main/java/com/example/psusweng/carcompanion/CarMaintHighLevel.java
@@ -3,7 +3,7 @@ package com.example.psusweng.carcompanion;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class CarMaintHighLevel extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
