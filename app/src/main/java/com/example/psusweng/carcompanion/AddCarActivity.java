72949e3c92a6b790131face0272769ed3070740b
tud12552 <jacob.s.esworthy@gmail.com>
2018-11-06 08:00:19 -0500

    Added the "onClickListener" events for buttons of the "AddCarActivity.java"                                                                         Fixed casting issues for all variables when using "findViewById" method.

--- app/src/main/java/com/example/psusweng/carcompanion/AddCarActivity.java
+++ app/src/main/java/com/example/psusweng/carcompanion/AddCarActivity.java
@@ -2,15 +2,48 @@ package com.example.psusweng.carcompanion;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 public class AddCarActivity extends AppCompatActivity
 {
+    private TextView mTxtViewScreenTitle = null;
+    private TextView mTxtViewMake = null;
+    private TextView mTxtViewYear = null;
+    private TextView mTxtViewModel = null;
+    private TextView mTxtViewMilage = null;
+    private TextView mTxtViewEstMilageYear = null;
+    private TextView mTxtViewUploadPhoto = null;
+    private TextView mTxtViewLasOilChange = null;
 
+    private Spinner mSpinMake = null;
+    private Spinner mSpinModel = null;
+
+    private EditText mEditTxtYear = null;
+    private EditText mEditTxtMilage = null;
+    private EditText mEditTxtOilChange = null;
+    private EditText mEditTxtEstMilesYear = null;
+
+    private ImageButton mImageBtnUploadPhoto = null;
+
+    private Button mBtnAddCar = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_car);
+
+        mSpinMake = (Spinner)findViewById(R.id.makeDropdown);
+        mSpinModel = (Spinner)findViewById(R.id.modelDropdown);
+
+        mEditTxtEstMilesYear = (EditText)findViewById(R.id.estMileageBox);
+        mEditTxtMilage = (EditText)findViewById(R.id.enterMileageBox);
+        mEditTxtOilChange = (EditText)findViewById(R.id.lastOilChangeBox);
+        mEditTxtYear = (EditText)findViewById(R.id.enterYearBox);
+
 
     }
 }
--- app/src/main/java/com/example/psusweng/carcompanion/CarSelectActivity.java
+++ app/src/main/java/com/example/psusweng/carcompanion/CarSelectActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.TextView;
 
@@ -16,19 +17,18 @@ public class CarSelectActivity extends AppCompatActivity {
     private ImageButton mImageBtnAddCar = null;
 
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_car_select);
 
 
-        mTxtViewScreenTitle = findViewById(R.id.txtViewScreenTitle);
-        mTxtViewCar = findViewById(R.id.viewCarText);
-        mTxtViewAddCar = findViewById(R.id.addCarText);
+        mTxtViewScreenTitle = (TextView) findViewById(R.id.txtViewScreenTitle);
+        mTxtViewCar = (TextView) findViewById(R.id.viewCarText);
+        mTxtViewAddCar = (TextView) findViewById(R.id.addCarText);
 
-        mImageBtnAddCar = findViewById(R.id.addCarButton);
-        mImageBtnViewCar = findViewById(R.id.viewCarButton);
+        mImageBtnAddCar = (ImageButton) findViewById(R.id.addCarButton);
+        mImageBtnViewCar = (ImageButton) findViewById(R.id.viewCarButton);
 
 
         mImageBtnViewCar.setOnClickListener(new View.OnClickListener() {
@@ -43,7 +43,7 @@ public class CarSelectActivity extends AppCompatActivity {
         mImageBtnAddCar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent goToAddCarScreen = new Intent(CarSelectActivity.this,AddCarActivity.class);
+                Intent goToAddCarScreen = new Intent(CarSelectActivity.this, AddCarActivity.class);
                 startActivity(goToAddCarScreen);
             }
         });
@@ -51,8 +51,7 @@ public class CarSelectActivity extends AppCompatActivity {
     }
 
     @Override
-    protected void onResume()
-    {
+    protected void onResume() {
         super.onResume();
     }
 
