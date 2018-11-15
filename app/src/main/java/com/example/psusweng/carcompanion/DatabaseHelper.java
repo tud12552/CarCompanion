24f1b42f88020657f9015aa9de951604e40e748d
tud12552 <jacob.s.esworthy@gmail.com>
2018-11-15 18:22:04 -0500

    Added the DatabaseHelper class to begin integration of SQLite database.  - Added the addNewCar method to add cars to the database.  - Added the getAllCars method to get all the cars currently in the database.  This can be used in the initial screen to display all cars the users enters as a ListView or ReceycleView.

---
+++ app/src/main/java/com/example/psusweng/carcompanion/DatabaseHelper.java
@@ -0,0 +1,84 @@
+package com.example.psusweng.carcompanion;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+import java.util.Date;
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+
+    public static final String TAG = "DatabaseHelper";
+
+    public static final String TABLE_NAME = "USER_CAR_INFO";
+    public static final String COL0 = "id";
+    public static final String COL1 = "year";
+    public static final String COL2 = "make";
+    public static final String COL3 = "model";
+    public static final String COL4 = "miles";
+    public static final String COL5 = "yearly_miles";
+    public static final String COL6 = "lat_oil_change";
+
+    public DatabaseHelper(Context context)
+    {
+        super(context, TABLE_NAME, null, 1);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db)
+    {
+        String createTable = "CREATE TABLE " + TABLE_NAME + " (" +
+                COL0 + " INTEGER PRIMARY KEY AUTOINCREMENT," + COL1 + " TEXT," + COL2 +
+                " TEXT," + COL3 +" TEXT," + COL4 +" TEXT," + COL5 +" TEXT," + COL6 +
+                "TEXT)";
+
+        db.execSQL(createTable);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int i, int ii)
+    {
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
+        onCreate(db);
+    }
+
+    public boolean addNewCar(String make, String model, String year, String miles, String yearlyMiles, String lastOilChange)
+    {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+
+        contentValues.put(COL1, year);
+        contentValues.put(COL2, make);
+        contentValues.put(COL3, model);
+        contentValues.put(COL4, miles);
+        contentValues.put(COL5, yearlyMiles);
+        contentValues.put(COL6, lastOilChange);
+
+        Log.d(TAG, "addNewCar: Adding " + year + " " + make + " " + model);
+
+        long result = db.insert(TABLE_NAME, null, contentValues);
+
+        if(result == -1)
+        {
+            return false;
+        }
+        else
+        {
+            return true;
+        }
+    }
+
+    public Cursor getAllCars()
+    {
+        SQLiteDatabase db = this.getWritableDatabase();
+        String query = "SELECT * FROM " + TABLE_NAME;
+
+        Cursor data = db.rawQuery(query, null);
+
+        return data;
+    }
+
+}
