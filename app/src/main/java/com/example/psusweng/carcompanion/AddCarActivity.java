3522b0bf98e7447054872a2cad59376bd65a6950
tud12552 <jacob.s.esworthy@gmail.com>
2018-11-15 20:06:19 -0500

    Continued SQLite Database integration. Still need to add the ListView to ViewCars and double check SQLite is working properly.

--- app/src/main/java/com/example/psusweng/carcompanion/AddCarActivity.java
+++ app/src/main/java/com/example/psusweng/carcompanion/AddCarActivity.java
@@ -2,11 +2,14 @@ package com.example.psusweng.carcompanion;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class AddCarActivity extends AppCompatActivity
 {
@@ -31,10 +34,16 @@ public class AddCarActivity extends AppCompatActivity
 
     private Button mBtnAddCar = null;
 
+    private String mSelectedMake;
+    private String mSelectedModel;
+
+    DatabaseHelper mDatabaseHelper;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_car);
+
 
         mSpinMake = (Spinner)findViewById(R.id.makeDropdown);
         mSpinModel = (Spinner)findViewById(R.id.modelDropdown);
@@ -44,13 +53,94 @@ public class AddCarActivity extends AppCompatActivity
         mEditTxtOilChange = (EditText)findViewById(R.id.lastOilChangeBox);
         mEditTxtYear = (EditText)findViewById(R.id.enterYearBox);
 
+        mBtnAddCar = (Button)findViewById(R.id.addCarBtn);
 
 
+        mDatabaseHelper = new DatabaseHelper(this);
+
+
+        mSpinMake.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                mSelectedMake = mSpinMake.getItemAtPosition(position).toString();
+                toastMessage("You chose " + mSelectedMake);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+        mSpinModel.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                mSelectedModel = mSpinModel.getItemAtPosition(position).toString();
+                toastMessage("You chose " + mSelectedModel);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+        mBtnAddCar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String make = mSelectedMake;
+                String model = mSelectedModel;
+                String year = mEditTxtYear.getText().toString();
+                String miles = mEditTxtMilage.getText().toString();
+                String yearlyMileage = mEditTxtEstMilesYear.getText().toString();
+                String lastOilChange = mEditTxtOilChange.getText().toString();
+//                String[] NewCar = new String[]{make, model, year, miles, yearlyMileage, lastOilChange};
+//
+//                Boolean okToAdd = false;
+//
+//                for (int i = 0; i <= NewCar.length; i++)
+//                {
+//                    if (NewCar[i].isEmpty()) {
+//                        okToAdd = false;
+//                        break;
+//                    } else {
+//                        okToAdd = true;
+//                    }
+//                }
+//                if(okToAdd)
+//                {
+                    AddNewCar(make,model,year,miles,yearlyMileage,lastOilChange);
+//                }
+//                else
+//                {
+//                    toastMessage("You must inset a name.");
+//                }
+            }
+        });
+    }
+
+    public void AddNewCar(String make, String model, String year, String miles, String YearlyMiles, String LastOilChange)
+    {
+        boolean insertData = mDatabaseHelper.addNewCar(make, model, year, miles, YearlyMiles, LastOilChange);
+
+        if(insertData)
+        {
+            toastMessage("Car Successfully Added.");
+        }
+        else
+        {
+            toastMessage("Car Was Not Added.");
+        }
     }
 
     @Override
     protected void onResume()
     {
         super.onResume();
+    }
+
+    public void toastMessage(String msg)
+    {
+        Toast.makeText(getApplicationContext(), msg,Toast.LENGTH_SHORT).show();
     }
 }
--- app/src/main/java/com/example/psusweng/carcompanion/DatabaseHelper.java
+++ app/src/main/java/com/example/psusweng/carcompanion/DatabaseHelper.java
@@ -6,6 +6,7 @@ import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.util.Log;
+import android.widget.Toast;
 
 import java.util.Date;
 
@@ -80,5 +81,4 @@ public class DatabaseHelper extends SQLiteOpenHelper {
 
         return data;
     }
-
 }
